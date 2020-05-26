3c1c4ed573a55ff8fe61b5c2bacdd5391bdcb3ec
Gisele <giselesousar@gmail.com>
2020-05-25 22:33:12 -0300

    add componente base

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import Home from './pages/Home';
+import BottomMenu from './pages/Componentes/BottomNavigation';
 
 
 
@@ -11,8 +12,7 @@ export default function Routes(){
     return(
         <NavigationContainer>
             <AppStack.Navigator screenOptions={{headerShown:false}}>
-                <AppStack.Screen name ="Home" component={Home}/>
-                
+                <AppStack.Screen name ="Home" component={BottomMenu}/>
             </AppStack.Navigator>
         </NavigationContainer>
 
