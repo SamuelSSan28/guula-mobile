98bfe42feeb26009554b51a36e8cd8641d5dc528
Gisele <giselesousar@gmail.com>
2020-06-06 19:25:21 -0300

    add infinite scroll

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,16 +2,23 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import api from '../../services/api.js'
 import Header_Base from '../Componentes/Header_Base';
-
 import Card_Component from '../Componentes/Card';
 
 
 export default function HomeScreen(){
   const [recipes, setRecipes] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   async function loadRecipes(){
-    const res = await api.get("recipes/random/8").catch(function(err){alert(err)});
-    setRecipes(res.data);
+    if(loading){
+      return;
+    }
+    setLoading(true);
+    const res = await api.get(`recipes?page=${page}`).catch(function(err){alert(err)});
+    setRecipes([...recipes,...res.data]);
+    setPage(page+1);
+    setLoading(false);
   }
   useEffect( () => {loadRecipes()}, []); 
 
@@ -26,7 +33,7 @@ export default function HomeScreen(){
   return(
     <>
     <Header_Base/>
-    <Card_Component receitas={recipes}/> 
+    <Card_Component receitas={recipes} func={loadRecipes} loading={loading}/>  
     </>
     )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create(
     {
-        baseURL:'https://guula-backend.herokuapp.com/',
+        //baseURL:'https://guula-backend.herokuapp.com/',
+        baseURL:'http://192.168.0.113:3333'
     }
 );
 
