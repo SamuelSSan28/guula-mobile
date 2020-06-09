cc72aba956550073abf840a030d9e703e64b5581
Gisele <giselesousar@gmail.com>
2020-06-09 10:11:12 -0300

    add Alert component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,19 +3,22 @@ import { useState, useEffect } from 'react';
 import api from '../../services/api.js'
 import Header_Base from '../Componentes/Header_Base';
 import Card_Component from '../Componentes/Card';
+import Alert from '../Componentes/Alert';
 
 
 export default function HomeScreen(){
   const [recipes, setRecipes] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertContent, setAlertContent] = useState('');
 
   async function loadRecipes(){
     if(loading){
       return;
     }
     setLoading(true);
-    const res = await api.get(`recipes?page=${page}`).catch(function(err){alert(err)});
+    const res = await api.get(`recipes?page=${page}`).catch(function(err){setShowAlert(true), setAlertContent(err)});
     const receitas = res.data;
     for (let i = receitas.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -39,6 +42,7 @@ export default function HomeScreen(){
     <>
     <Header_Base/>
     <Card_Component receitas={recipes} func={loadRecipes} loading={loading}/>  
+    {showAlert && <Alert content={alertContent} setShowAlert={setShowAlert}/>}
     </>
     )
 }
